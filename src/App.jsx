6e6c4f8f25a29b0b20916ce93523dc2660915e49
Jeniffer Steinback <jeniffer.asnasciment@gmail.com>
2025-04-07 21:20:35 -0300

    React

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { Contato } from "./pages/Contato"
-import { Route, Router, Routes } from "react-router-dom"
+
 
 function App(){
 
